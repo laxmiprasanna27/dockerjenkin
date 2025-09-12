e2a4b0dba7ac11bd86fa916c61cba430e4157f4d
laxmiprasanna27 <154531062+laxmiprasanna27@users.noreply.github.com>
2025-09-12 10:55:00 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template, request
+app=Flask(_name_)
+@app.route('/')
+def index():
+    return render_template("form.html")
+@app.route('/submit',methods=['POST'])
+def submit():
+    username=request.form.get('username')
+    email=request.form.get('email')
+    year=request.form.get('year')
+    phone=request.form.get('phone')
+    return render_template('result.html',name=username,email=email,year=year,phone=phone)
+if _name_ == "_main_":
+    app.run(host='0.0.0.0',port="5001",debug=True)
+
